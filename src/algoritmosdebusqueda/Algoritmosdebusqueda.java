7a3cb4a5c366fbf27293cfc086201bfc03e42ea3
Daniel Prieto <danielprieto052003@gmail.com>
2023-03-23 11:41:05 -0500

    first commit

---
+++ src/algoritmosdebusqueda/Algoritmosdebusqueda.java
@@ -0,0 +1,37 @@
+package algoritmosdebusqueda;
+
+ // @author DanielPb05
+public class Algoritmosdebusqueda {
+
+    public static void main(String[] args) {
+        int lista[] =new int [5];
+        
+        //secuencia
+        lista[0]=15;
+        lista[1]=0;
+        lista[2]=25;
+        lista[3]=10;
+        lista[4]=6;
+        int valorabuscar=36;
+        boolean esEncontrado= false;
+        for(int i =0; (i<lista.length)&&(!esEncontrado) ; i++){
+        if (lista[i]==valorabuscar){
+            System.out.println("El valor se encontroEl valor se encontro");
+             
+  
+        }else
+        {
+         System.out.println("El valor no  se encontro");
+        
+        }
+        }
+    }
+}
+             
+
+      
+        
+
+                
+        
+               
